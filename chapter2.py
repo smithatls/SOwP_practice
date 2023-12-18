1e14501ad03fbf7b4783090a7c20f334451161e1
Никита Кузнецов <kuznetsovnv2002@gmail.com>
2023-12-18 20:33:18 +0300

    2

---
+++ chapter2.py
@@ -0,0 +1,8 @@
+# # 2
+# INCOME = 0.23
+# output = int(input("Enter a "))
+# print(f'{output * INCOME}')
+# 3
+ACRES_IN_METERS = 4046.86
+square_meters = float(input())
+print(f'{}')
---
+++ test.py
@@ -0,0 +1,4 @@
+# sales = 3.13924
+# print(f'{sales: .2f}')
+# sales = 3123123.13924
+# print(f'{sales: ,.1f} and ', 'more', sep='@')
