ee5c374d4ee72f94841ee6458af8659fc3f74ee9
Никита Кузнецов <kuznetsovnv2002@gmail.com>
2023-12-19 22:44:02 +0300

    2

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-# sales = 3.13924
-# print(f'{sales: .2f}')
-# sales = 3123123.13924
-# print(f'{sales: ,.1f} and ', 'more', sep='@')
+m1 = int(input())
+m2 = int(input())
+m3 = int(input())
+r = 5 * m1 + 10 * m2 + 50 * m3
+if r == 100:
+    print('good')
+elif r > 100:
+    print('1')
+elif r < 100:
+    print('2')
+
